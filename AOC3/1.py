c6332606f804a3dbaf1fccbd9cfd31185f1c40d5
Pisdrul <FRANCESCO.TREVISAN@studenti.units.it>
2022-12-07 16:09:10 +0100

    Update 1.py

--- AOC3/1.py
+++ AOC3/1.py
@@ -10,27 +10,30 @@ def dividiZaino(zaino): #divida la stringa in 2
     grandezza=int(len(zaino)) +1
     split=grandezza/2 -1
     primaParte=zaino[0:int(split)]
-    secondaParte=zaino[int(split)  :grandezza]
+    secondaParte=zaino[int(split):grandezza]
     return primaParte,secondaParte
 
 def iteraString(primaParte,secondaParte):
+    c=''
     for lettera in primaParte:
         c=controllaDuplicati(lettera,secondaParte)
-        if (c!=' '):
-            print(c)
+        if (c != None):
             return c
+
 
 def controllaDuplicati(carattere,zaino):
     for lettera in zaino:
         if(carattere == lettera):
             return carattere
-            break
 
 with open('input.txt', 'r', encoding="utf-8") as f:
     lines = f.readlines()
     priorità=0
+    count=0
     for line in lines:
+        count+=1
         zaino= line
         primo,secondo=dividiZaino(zaino)
-        priorità+=Priority(iteraString(primo,secondo))
-        print(priorità)
+        carattere=iteraString(primo,secondo)
+        priorità+=Priority(carattere)
+    print(priorità)
